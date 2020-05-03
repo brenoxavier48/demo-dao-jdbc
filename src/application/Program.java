14254ae755d40ca62ced1fcd4e6e3b6e953dab9b
BrenoXavier <brenoxavier8.bx@gmail.com>
2020-05-03 11:41:48 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj.hashCode());
+		
+		Seller seller = new Seller(21, "Breno", "@filho da puta", new Date(), 4511.5, obj);
+		System.out.println(obj+"\n"+seller);
 	}
 
 }
